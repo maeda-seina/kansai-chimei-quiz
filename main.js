dc48d9656075676c407d7bd712453a3c77cff9c7
maeda-seina <ayrton19601027@gmail.com>
2021-12-12 23:29:48 +0900

    出題の順序をランダムに変更(arrayShuffleメソッドを追加)

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 const data = require('./data')
 const { Quiz } = require('enquirer')
 // 0~10の間で得たい場合は、11を指定
-const randomNumber = Math.floor(Math.random() * 10)
+const randomNumber = Math.floor(Math.random() * 1)
 const readline = require('readline')
 
 class SetQuiz {
@@ -14,13 +14,23 @@ class SetQuiz {
   }
 
   explain () {
-    console.log('関西の難読地名クイズを行います。\n始めるには何かクリックしてください。')
+    console.log('関西の難読地名クイズを行います。\n始めるには何かキーを入力してください。')
   }
 
   wait () {
     readline.emitKeypressEvents(process.stdin)
     process.stdin.setRawMode(true)
     process.stdin.resume()
+  }
+
+  arrayShuffle (array) {
+    for (let i = (array.length - 1); i > 0; i--) {
+      const randomNumber = Math.floor(Math.random() * (i + 1))
+      const temporaryNumber = array[i]
+      array[i] = array[randomNumber]
+      array[randomNumber] = temporaryNumber
+    }
+    return array
   }
 
   async select (question) {
@@ -41,14 +51,14 @@ class SetQuiz {
     this.wait()
     let point = 0
     process.stdin.once('data', async () => {
-      for (const question of data.questions[randomNumber]) {
+      for (const question of this.arrayShuffle(data.questions[randomNumber])) {
         await this.select(question)
         point += this.point_
       }
       if (point >= 80) {
         console.log(`おめでとうございます、合格です！！点数は${point}点でした。\nお疲れ様でした。`)
       } else {
-        console.log(`不合格です。復習してもう一度挑んでください。点数は${point}点でした\nお疲れ様でした。`)
+        console.log(`不合格です。点数は${point}点でした。復習してもう一度挑んでください。\nお疲れ様でした。`)
       }
     })
   }
